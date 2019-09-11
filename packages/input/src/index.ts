b89426fd2bae31de0f8e97762ab9200507325de2
Thanh Nguyen <thanhnguyencs@outlook.com>
2019-09-10 19:19:53 -0500

    add new type

--- packages/input/src/index.ts
+++ packages/input/src/index.ts
@@ -1,4 +1,7 @@
 import { InputProps } from './Input';
+import TestType from './type';
 
 export { Input } from './Input';
 export type InputType = InputProps;
+
+export type typeTemp = TestType;
---
+++ packages/input/src/type.ts
@@ -0,0 +1,6 @@
+interface TestType {
+    abc: string;
+    numb: number;
+}
+
+export default TestType;
