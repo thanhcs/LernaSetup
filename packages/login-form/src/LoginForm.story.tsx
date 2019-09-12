997445e9e2936e01572010026dd221ce489824be
Thanh Nguyen <thanhnguyencs@outlook.com>
2019-09-12 05:41:37 -0500

    Add Story with Router and resolve by decorator

--- packages/login-form/src/LoginForm.story.tsx
+++ packages/login-form/src/LoginForm.story.tsx
@@ -2,8 +2,13 @@ import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 import { LoginForm } from './LoginForm';
+import ComponentParams from './ComponentParams';
+import { MemoryRouter as Router } from 'react-router-dom';
 
-storiesOf('LoginForm', module).add('default', () => {
-    const onClick = action('logIn clicked');
-    return <LoginForm onClick={onClick} />;
-});
+storiesOf('LoginForm', module)
+    .addDecorator(story => <Router initialEntries={['/foo']}>{story()}</Router>)
+    .add('default', () => {
+        const onClick = action('logIn clicked');
+        return <LoginForm onClick={onClick} />;
+    })
+    .add('Test Router', () => <ComponentParams />);
